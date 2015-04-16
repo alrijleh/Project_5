69118fb606476fc09592ba3b7f354c4f3a65a9a6
Fouad Al-Rijleh <alrijleh@gmail.com>
2015-04-16 16:18:29 -0400

    creating msfPrim()

--- Project 5/main.cpp
+++ Project 5/main.cpp
@@ -32,19 +32,19 @@ typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgePropert
 
 struct VertexProperties
 {
-   pair<int,int> cell; // maze cell (x,y) value
-   Graph::vertex_descriptor pred;
-   bool visited;
-   bool marked;
-   int weight;
+	pair<int, int> cell; // maze cell (x,y) value
+	Graph::vertex_descriptor pred;
+	bool visited;
+	bool marked;
+	int weight;
 };
 
 // Create a struct to hold properties for each edge
 struct EdgeProperties
 {
-   int weight;
-   bool visited;
-   bool marked;
+	int weight;
+	bool visited;
+	bool marked;
 };
 
 typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
@@ -132,7 +132,7 @@ bool isConnected(Graph &g)
 bool subCycle(Graph &g, pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange)
 {
 	if (num_vertices(g) == 0) return false;
-	
+
 	Graph::vertex_iterator vItr = vItrRange.first;
 	stack<Graph::vertex_descriptor> stack;
 	stack.push(*vItr);
@@ -232,134 +232,167 @@ void findSpanningForest(Graph &g, Graph &sf)
 }
 
 void initializeGraph(Graph &g,
-                     Graph::vertex_descriptor &start,
-                     Graph::vertex_descriptor &end, ifstream &fin)
-// Initialize g using data from fin.  Set start and end equal
-// to the start and end nodes.
+	Graph::vertex_descriptor &start,
+	Graph::vertex_descriptor &end, ifstream &fin)
+	// Initialize g using data from fin.  Set start and end equal
+	// to the start and end nodes.
 {
-   EdgeProperties e;
-   
-   int n, i, j;
-   int startId, endId;
-   fin >> n;
-   fin >> startId >> endId;
-   Graph::vertex_descriptor v;
-   
-   // Add nodes.
-   for (int i = 0; i < n; i++)
-   {
-      v = add_vertex(g);
-      if (i == startId)
-         start = v;
-      if (i == endId)
-         end = v;
-   }
-   
-   while (fin.peek() != '.')
-   {
-      fin >> i >> j >> e.weight;
-      add_edge(i,j,e,g);
-   }
+	EdgeProperties e;
+
+	int n, i, j;
+	int startId, endId;
+	fin >> n;
+	fin >> startId >> endId;
+	Graph::vertex_descriptor v;
+
+	// Add nodes.
+	for (int i = 0; i < n; i++)
+	{
+		v = add_vertex(g);
+		if (i == startId)
+			start = v;
+		if (i == endId)
+			end = v;
+	}
+
+	while (fin.peek() != '.')
+	{
+		fin >> i >> j >> e.weight;
+		add_edge(i, j, e, g);
+	}
+}
+
+void msfPrim(Graph &g, Graph &sf)
+{
+	clearVisited(g);
+	g[0].visited = true;
+	pair<Graph::edge_descriptor, bool> currentPair;
+	Graph::edge_descriptor cheapestEdge, currentEdge;
+	currentEdge = *edges(g).first; //initialize current edge to first edge -- doesnt matter which
+	cheapestEdge = *edges(g).first; //initialize cheapest edge to first edge -- doesnt matter which
+	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+	for (Graph::vertex_iterator u = vItrRange.first; u != vItrRange.second; ++u)
+	{
+		if (g[*u].visited == true)
+			for (Graph::vertex_iterator v = vItrRange.first; v != vItrRange.second; ++v)
+			{
+				if (g[*v].visited == false)
+				{
+					//current edge goes from marked u to unmarked v
+					currentPair = edge(*u, *v, g);
+					currentEdge = currentPair.first;
+					if (currentPair.second == true
+						&& g[currentEdge].weight <= g[cheapestEdge].weight)
+						cheapestEdge = currentEdge;
+				}
+			}
+		Graph::vertex_descriptor i = source(currentEdge, g);
+		Graph::vertex_descriptor j = target(currentEdge, g);
+		add_edge(i, j, sf);
+		add_edge(j, i, sf);
+		g[j].visited == true;
+	}
 }
 
 int main()
 {
-   char x;
-   ifstream fin;
-   stack <int> moves;
-   string fileName;
-   
-   // Read the name of the graph from the keyboard or
-   // hard code it here for testing.
-   
-   fileName = "graph1.txt";
-   
-   //   cout << "Enter filename" << endl;
-   //   cin >> fileName;
-   
-   fin.open(fileName.c_str());
-   if (!fin)
-   {
-      cerr << "Cannot open " << fileName << endl;
-      exit(1);
-   }
-   
-   try
-   
-   {
-      cout << "Reading graph" << endl;
-      Graph g;
-      
-      Graph::vertex_descriptor start,end;
+	char x;
+	ifstream fin;
+	stack <int> moves;
+	string fileName;
 
-      initializeGraph(g,start,end,fin);
-      cout << "Num nodes: " << num_vertices(g) << endl;
-      cout << "Num edges: " << num_edges(g) << endl;
-      cout << endl;
-      
-      // cout << g;
-      
-      bool connected;
-      bool cyclic;
-      
-      cout << "Calling isCyclic" << endl;
-      cyclic = isCyclic(g);
-      
-      if (cyclic)
-         cout << "Graph contains a cycle" << endl;
-      else
-         cout << "Graph does not contain a cycle" << endl;
-      
-      cout << endl;
-      
-      cout << "Calling isConnected" << endl;
-      connected = isConnected(g);
-      
-      if (connected)
-         cout << "Graph is connected" << endl;
-      else
-         cout << "Graph is not connected" << endl;
-      
-      cout << endl;
-      cout << "Finding spanning forest" << endl;
-      
-      // Initialize an empty graph to contain the spanning forest
-      Graph sf(num_vertices(g));
-      
-      findSpanningForest(g,sf);
-      
-	  cout << "Num nodes: " << num_vertices(sf) << endl;
-	  cout << "Num edges: " << num_edges(sf) << endl;
-	  cout << endl;
+	// Read the name of the graph from the keyboard or
+	// hard code it here for testing.
 
-      //cout << "Spanning forest weight: " << totalEdgeWeight(sf)/2 << endl;
-      //cout << endl;
-      
-      cout << "Calling isConnected" << endl;
-      connected = isConnected(sf);
-      
-      if (connected)
-         cout << "Graph is connected" << endl;
-      else
-         cout << "Graph is not connected" << endl;
-      cout << endl;
-      
-      cout << "Calling isCyclic" << endl;
-      cyclic = isCyclic(sf);
-      
-      if (cyclic)
-         cout << "Graph contains a cycle" << endl;
-      else
-         cout << "Graph does not contain a cycle" << endl;
-      cout << endl;
-   }
-   catch (indexRangeError &ex)
-   { 
-      cout << ex.what() << endl; exit(1);
-   }
-   catch (rangeError &ex)
-   {
-      cout << ex.what() << endl; exit(1);
-   }
+	fileName = "graph1.txt";
+
+	//   cout << "Enter filename" << endl;
+	//   cin >> fileName;
+
+	fin.open(fileName.c_str());
+	if (!fin)
+	{
+		cerr << "Cannot open " << fileName << endl;
+		exit(1);
+	}
+
+	try
+
+	{
+		cout << "Reading graph" << endl;
+		Graph g;
+
+		Graph::vertex_descriptor start, end;
+
+		initializeGraph(g, start, end, fin);
+		cout << "Num nodes: " << num_vertices(g) << endl;
+		cout << "Num edges: " << num_edges(g) << endl;
+		cout << endl;
+
+		// cout << g;
+
+		bool connected;
+		bool cyclic;
+
+		cout << "Calling isCyclic" << endl;
+		cyclic = isCyclic(g);
+
+		if (cyclic)
+			cout << "Graph contains a cycle" << endl;
+		else
+			cout << "Graph does not contain a cycle" << endl;
+
+		cout << endl;
+
+		cout << "Calling isConnected" << endl;
+		connected = isConnected(g);
+
+		if (connected)
+			cout << "Graph is connected" << endl;
+		else
+			cout << "Graph is not connected" << endl;
+
+		cout << endl;
+		cout << "Finding spanning forest" << endl;
+
+		// Initialize an empty graph to contain the spanning forest
+		Graph sf(num_vertices(g));
+
+		//findSpanningForest(g, sf);
+		msfPrim(g, sf);
+
+		cout << "Num nodes: " << num_vertices(sf) << endl;
+		cout << "Num edges: " << num_edges(sf) << endl;
+		cout << endl;
+
+		//cout << "Spanning forest weight: " << totalEdgeWeight(sf)/2 << endl;
+		//cout << endl;
+
+		cout << "Calling isConnected" << endl;
+		connected = isConnected(sf);
+
+		if (connected)
+			cout << "Graph is connected" << endl;
+		else
+			cout << "Graph is not connected" << endl;
+		cout << endl;
+
+		cout << "Calling isCyclic" << endl;
+		cyclic = isCyclic(sf);
+
+		if (cyclic)
+			cout << "Graph contains a cycle" << endl;
+		else
+			cout << "Graph does not contain a cycle" << endl;
+		cout << endl;
+	}
+	catch (indexRangeError &ex)
+	{
+		cout << ex.what() << endl; exit(1);
+	}
+	catch (rangeError &ex)
+	{
+		cout << ex.what() << endl; exit(1);
+	}
 }
 
